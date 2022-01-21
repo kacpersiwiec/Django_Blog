a77ef0173a37a2b25b625874f4de60d4ec10bc84
Kacper Siwiec <kacpersiwiec00@interia.pl>
2022-01-21 19:15:47 +0100

    initial

---
+++ blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'slug', 'publish', 'status')
+    list_filter = ('created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    populated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('publish',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
